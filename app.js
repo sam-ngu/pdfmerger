5aff5e57eb6728b6eb79fefae243c9073877b1d7
Sam <opensource@acadea.io>
2021-02-03 16:51:13 +0800

    ignore .DStore

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const PDFMerger = require("pdf-merger-js");
 
 const merger = new PDFMerger();
 
-const files = fs.readdirSync("inputs");
+const files = fs.readdirSync("inputs").filter((file) => file !== '.DS_Store');
 
 function merge() {
     for (let index = 0; index < files.length; index++) {
+        
         const file = path.join( __dirname, 'inputs', files[index]);
 
         console.log(file);
